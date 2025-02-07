bf8764fefc7857cd1b6b0768dc880c2048e02e34
IgorRudenko108 <88731084+IgorRudenko108@users.noreply.github.com>
2025-02-07 16:57:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,19 +2,15 @@ const searchButton = document.getElementById('searchButton');
 const searchInputBox = document.getElementById('searchInputBox');
 const searchInput = document.getElementById('searchInput');
 
-// Відкриваємо поле вводу при натисканні на кнопку
 searchButton.addEventListener('click', () => {
     if (searchInputBox.classList.contains('visible')) {
-        // Якщо поле вводу вже відкрите, очищуємо його
-        searchInput.value = ''; // Очищаємо поле вводу
+        searchInput.value = '';
     } else {
-        // Якщо поле вводу не відкрите, відкриваємо його
         searchInputBox.classList.add('visible');
-        searchInput.focus();  // Фокус на поле вводу після відкриття
+        searchInput.focus();
     }
 });
 
-// Закриваємо поле вводу, якщо клікнути за межами
 document.addEventListener('click', (event) => {
     if (!searchButton.contains(event.target) && !searchInputBox.contains(event.target)) {
         searchInputBox.classList.remove('visible');
@@ -27,23 +23,29 @@ const searchButton2 = document.getElementById('searchButton2');
 const searchInputBox2 = document.getElementById('searchInputBox2');
 const searchInput2 = document.getElementById('searchInput2');
 
-// Відкриваємо поле вводу при натисканні на кнопку
 searchButton2.addEventListener('click', () => {
     if (searchInputBox2.classList.contains('visible')) {
-        // Якщо поле вводу вже відкрите, очищуємо його
-        searchInput2.value = ''; // Очищаємо поле вводу
+        searchInput2.value = ''; 
     } else {
-        // Якщо поле вводу не відкрите, відкриваємо його
         searchInputBox2.classList.add('visible');
-        searchInput2.focus();  // Фокус на поле вводу після відкриття
+        searchInput2.focus(); 
     }
 });
 
-// Закриваємо поле вводу, якщо клікнути за межами
 document.addEventListener('click', (event) => {
     if (!searchButton2.contains(event.target) && !searchInputBox2.contains(event.target)) {
         searchInputBox2.classList.remove('visible');
     }
 });
-
 // ----------------------------------------------------------------------
+
+
+window.addEventListener("scroll", function () {
+    const header = document.getElementById("site-header");
+    if (window.scrollY > 50) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+  
